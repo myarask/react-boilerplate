1ed40576e0a4dafe168c1cfa5a1c5c41559e16b5
myarask <mark@yaraskavitch.com>
2019-08-27 23:23:25 -0400

    Attempt a grid

--- src/pages/Home/HomeDesktop.jsx
+++ src/pages/Home/HomeDesktop.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import { Card, CardContent, Typography, TextField } from '@material-ui/core';
 
+const Thing = () => (
+  <div
+    style={{
+      width: '150px',
+      background: '#EC985A',
+      color: 'white',
+      margin: '0 1rem 1rem 0',
+      display: 'inline-block',
+      height: `${Math.random() * (700 - 200) + 200}px`,
+    }}
+  >
+    1{' '}
+  </div>
+);
+
 const HomeDesktop = props => (
-  <div style={{ background: '#eee', height: '100%', display: 'flex' }}>
-    <div style={{ width: '300px', padding: '1rem' }}>
+  <div
+    style={{
+      background: '#eee',
+      height: '100%',
+      display: 'grid',
+      gridTemplateColumns: '300px auto',
+    }}
+  >
+    <div style={{ padding: '1rem' }}>
       <Card>
         <CardContent>
           <Typography>Find your shit!</Typography>
@@ -19,7 +41,17 @@ const HomeDesktop = props => (
         </CardContent>
       </Card>
     </div>
-    <div>fdsfd</div>
+    <div style={{ columns: '6 200px', columnGap: '1rem', overflowY: 'scroll' }}>
+      <Thing />
+      <Thing />
+      <Thing />
+      <Thing />
+      <Thing />
+      <Thing />
+      <Thing />
+      <Thing />
+      <Thing />
+    </div>
   </div>
 );
 
